93d5a08668e001c8bf0dc5886bcd47b896c77079
“Samara <samara.limads@gmail.com>
2026-02-09 21:34:07 +0100

    added custom fonts and color sets

--- Together/ContentView.swift
+++ Together/ContentView.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.custom("IvyJournal-SemiBoldItalic", size: 25))
+            
+            
+            
         }
         .padding()
     }
 }
+
+
 
 #Preview {
     ContentView()
